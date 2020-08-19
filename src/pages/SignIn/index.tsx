6544f0a8e257bbdf5d278483c2321326a63edef8
JosephMartins <neto-neon@live.com>
2020-08-19 03:42:31 -0300

    Subindo implementaçao de criar nova conta e esqueci minha senha

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,30 +1,64 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
-
-    <Title>Faça seu logon</Title>
-
-    <Input name="email" icon="mail" placeholder="E-mail" />
-    <Input name="password" icon="lock" placeholder="Senha" />
-
-    <Button
-      onPress={() => {
-        console.log('funfou');
-      }}
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
     >
-      Entrar
-    </Button>
-  </Container>
+      <Container>
+        <Image source={logoImg} />
+        <View>
+          <Title>Faça seu logon</Title>
+        </View>
+
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+
+        <Button
+          onPress={() => {
+            console.log('funfou');
+          }}
+        >
+          Entrar
+        </Button>
+
+        <ForgotPassword
+          onPress={() => {
+            console.log('funfou');
+          }}
+        >
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+
+      <CreateAccountButton
+        onPress={() => {
+          console.log('funfou');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </KeyboardAvoidingView>
+  </>
 );
 
 export default SignIn;
